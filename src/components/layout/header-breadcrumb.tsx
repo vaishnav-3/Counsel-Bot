db200c0e7364e21cc8701c446602f6441ae6d80d
Vaishnav-3 <vaishnavpatil003@gmail.com>
2025-09-07 18:40:41 +0530

    re-organized components

---
+++ src/components/layout/header-breadcrumb.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { useChat } from "../provider/chat-context";
+
+export function HeaderBreadcrumb() {
+  const { selectedChatName } = useChat();
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbPage className="text-pretty">
+            {selectedChatName || "New chat"}
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
